4b0fc01ac0e4cdbb71de67e21560cabb15a04661
Will <doggy.huang@gmail.com>
2018-02-22 01:16:25 +0800

    CatchAll Demo for ASP.NET MVC 5 & ASP.NET Core 2 MVC

---
+++ Core2MVC/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core2MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("{*path}")]
+        public IActionResult Index(string path)
+        {
+            var url = Url.Action("Index", new { path = "hello/world" });
+            return Content("Path = [" + path + "], built URL = " + url);
+        }
+    }
+}
---
+++ MVC5/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string path)
+        {
+            var url = Url.Action("Index", new { path = "hello/world" });
+            return Content("Path = [" + path + "], built URL = " + url);
+        }
+    }
+}
